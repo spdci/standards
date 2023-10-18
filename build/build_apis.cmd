05f7e52b29df2147d3a91ea0b8078e888b485d00
Tameem Bin Haider <haidertameem@gmail.com>
2023-10-18 14:00:36 +0600

    Add the build instruction for jwks

--- build/build_apis.cmd
+++ build/build_apis.cmd
@@ -4,10 +4,12 @@
 
 swagger-cli -f 2 -t yaml bundle ./src/authz/authz_core_api_v1.0.0.yaml -o ./release/yaml/authz_core_api_v1.0.0.yaml
 swagger-cli -f 2 -t yaml bundle ./src/registry/registry_core_api_v1.0.0.yaml -o ./release/yaml/registry_core_api_v1.0.0.yaml
+swagger-cli -f 2 -t yaml bundle ./src/jwks/jwks_core_api_v1.0.0.yaml -o ./release/yaml/jwks_core_api_v1.0.0.yaml
 # swagger-cli -f 2 -t yaml bundle ./src/mapper/mapper_core_api_v1.0.0.yaml -o ./release/yaml/mapper_core_api_v1.0.0.yaml
 # swagger-cli -f 2 -t yaml bundle ./src/disburse/disburse_core_api_v1.0.0.yaml -o ./release/yaml/disburse_core_api_v1.0.0.yaml
 
 redocly build-docs ./release/yaml/authz_core_api_v1.0.0.yaml  -o ./release/html/authz_core_api_v1.0.0.html
 redocly build-docs ./release/yaml/registry_core_api_v1.0.0.yaml  -o ./release/html/registry_core_api_v1.0.0.html
+redocly build-docs ./release/yaml/jwks_core_api_v1.0.0.yaml  -o ./release/html/jwks_core_api_v1.0.0.html
 # redocly build-docs ./release/yaml/mapper_core_api_v1.0.0.yaml  -o ./release/html/mapper_core_api_v1.0.0.html
 # redocly build-docs ./release/yaml/disburse_core_api_v1.0.0.yaml  -o ./release/html/disburse_core_api_v1.0.0.html
